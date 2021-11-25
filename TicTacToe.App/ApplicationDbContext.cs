b9d41bd495f6c3ef7220db1b0bf50b62d8a6acc4
smc-dotexe <shanelgmcguire@gmail.com>
2021-11-24 21:14:27 -0700

    first commit

---
+++ TicTacToe.App/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.App
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        {
+        }
+
+        //public DbSet<> Games { get; set; }
+        //public DbSet<> Players { get; set; }
+
+    }
+}
